a955a69720f3f9cd33b128e7b3ccbfe32a9f52a7
rahuljoshi027 <rahul.joshi027@gmail.com>
2022-10-18 22:44:14 +0200

    add open api generator and generated files

--- shorturl/build.gradle.kts
+++ shorturl/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.0.14.RELEASE"
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
+	id("org.openapi.generator") version "6.2.0"
 }
 
 group = "com.shorturl"
@@ -21,7 +22,30 @@ dependencies {
 	implementation("org.apache.kafka:kafka-streams")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	// swagger open api
+	implementation("io.swagger.core.v3:swagger-annotations:2.2.3")
+	implementation("io.swagger.parser.v3:swagger-parser:2.1.3")
+	implementation("io.springfox:springfox-swagger-ui:3.0.0")
+	implementation("io.springfox:springfox-swagger2:3.0.0")
+	implementation("org.springdoc:springdoc-openapi-data-rest:1.6.11")
+	implementation("org.springdoc:springdoc-openapi-ui:1.6.11")
+	implementation("org.springdoc:springdoc-openapi-kotlin:1.6.11")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+openApiGenerate {
+	generatorName.set("kotlin-spring")
+	inputSpec.set("${projectDir}/src/main/resources/openapi.yml")
+	apiPackage.set("com.shorturl.generated.api")
+	modelPackage.set("com.shorturl.generated.model")
+	outputDir.set("$projectDir")
+	configOptions.put("interfaceOnly", "true")
+	configOptions.put("gradleBuildFile", "false")
+	configOptions.put("delegatePattern", "false")
+	configOptions.put("documentationProvider", "none")
+	configOptions.put("sourceFolder", "src/main/kotlin")
 }
 
 tasks.withType<KotlinCompile> {
---
+++ shorturl/src/main/kotlin/com/shorturl/generated/model/MakeShortUrlResponse.kt
@@ -0,0 +1,25 @@
+package com.shorturl.generated.model
+
+import java.util.Objects
+import com.fasterxml.jackson.annotation.JsonProperty
+import javax.validation.constraints.DecimalMax
+import javax.validation.constraints.DecimalMin
+import javax.validation.constraints.Email
+import javax.validation.constraints.Max
+import javax.validation.constraints.Min
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Pattern
+import javax.validation.constraints.Size
+import javax.validation.Valid
+
+/**
+ * coantain short url
+ * @param shortUrl 
+ */
+data class MakeShortUrlResponse(
+
+    @field:JsonProperty("shortUrl") val shortUrl: kotlin.String? = null
+) {
+
+}
+
