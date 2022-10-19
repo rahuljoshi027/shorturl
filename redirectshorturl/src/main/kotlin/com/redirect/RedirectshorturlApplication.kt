e6dae4ad0bb7f1c442ef1ec3a0d71f0f96dd4f83
rahuljoshi027 <rahul.joshi027@gmail.com>
2022-10-19 17:59:36 +0200

    add redirection service for short url

---
+++ redirectshorturl/src/main/kotlin/com/redirect/RedirectshorturlApplication.kt
@@ -0,0 +1,11 @@
+package com.redirect
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RedirectshorturlApplication
+
+fun main(args: Array<String>) {
+	runApplication<RedirectshorturlApplication>(*args)
+}
---
+++ redirectshorturl/src/test/kotlin/com/redirect/RedirectshorturlApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.redirect
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RedirectshorturlApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
