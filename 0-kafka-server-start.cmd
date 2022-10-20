50d0a3f4948b1f4d15a083e00d35b4e6fa321719
rahuljoshi027 <rahul.joshi027@gmail.com>
2022-10-20 02:28:18 +0200

    update script

--- 0-kafka-server-start.cmd
+++ 0-kafka-server-start.cmd
@@ -1,1 +1,1 @@
-kafka-server-start.bat C:\kafka_2.12-2.5.0\kafka_2.12-2.5.0\config\server-0.properties
+kafka-server-start.bat C:%kafka_home%\config\server-0.properties
--- zookeeper-start.cmd
+++ zookeeper-start.cmd
@@ -1,1 +1,1 @@
-zookeeper-server-start.bat C:\kafka_2.12-2.5.0\kafka_2.12-2.5.0\config\zookeeper.properties
+zookeeper-server-start.bat C:%kafka_home%\config\zookeeper.properties
